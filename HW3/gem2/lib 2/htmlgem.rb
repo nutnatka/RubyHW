dec8ac56f5dc1c60710d4e9f7444bf459b4617e2
nutnatka <93792379+nutnatka@users.noreply.github.com>
2021-12-04 23:05:37 +0200

    fix bugs #gem2

--- HW3/gem2/lib 2/htmlgem.rb
+++ HW3/gem2/lib 2/htmlgem.rb
@@ -1,5 +1,20 @@
+def save_content(content, file_name = "index.html")
+  html_template = "<!doctype html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport'
+          content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+    <title>Document</title>
+</head>
+<body>
+<script>
+     setInterval(()=>{ window.location.reload() }, 2000)
+ </script>
+ #{content}
+</body>
+</html>"
 
-  def writen_file
-    File.write('foo.txt', 'bar')
-  end
-  puts writen_file
+  File.write(file_name, html_template)
+end
--- HW3/gem2/secondgem.gemspec
+++ HW3/gem2/secondgem.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
-  s.name        = 'firstgem'
+  s.name        = 'htmlgem'
   s.version     = '0.0.1'
   s.summary     = "extended method Integer !"
   s.description = "extended method Integer"
   s.authors     = ["Nataliya.Virotskaya"]
   s.email       = 'chernienko.natalka@gmail.com'
-  s.files       = ["lib/firstgem.rb"]
+  s.files       = ["lib/htmlgem.rb"]
   s.homepage    =
-    'https://github.com/nutnatka/RubyHW/commit/844f0b198a403152f57a79871b682bd274c994a2'
+    'https://github.com/nutnatka/RubyHW'
   s.license       = 'MIT'
 end
